435e411009a619842c950c9b0cc79a8f1664ce1f
eferm <emanuel@eferm.com>
2020-06-06 23:04:47 -0700

    update script for bulk responses

--- scripts/get_survey.py
+++ scripts/get_survey.py
@@ -2,11 +2,13 @@
 import os
 import sys
 import json
-import pathlib
 import argparse
+from pathlib import Path
 
 import dotenv
 import requests
+
+MAX_REQUESTS = 10
 
 
 parser = argparse.ArgumentParser()
@@ -15,7 +17,7 @@ parser.add_argument('-e', '--endpoint', help='API endpoint to hit',
                     metavar='STRING', choices=['details', 'responses'],
                     default='details')
 parser.add_argument('-t', '--token', help='the access token', metavar='STRING')
-parser.add_argument('-o', '--output', help='write response to file',
+parser.add_argument('-o', '--output', help='write response to location',
                     metavar='FILE')
 
 args = parser.parse_args()
@@ -35,18 +37,38 @@ headers = {
     'Content-Type': 'application/json',
 }
 
+results = []
+
 if args.endpoint == 'details':
     url = f'{host}/{args.survey_id}/details'
     response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    results.append(response.json())
 elif args.endpoint == 'responses':
-    url = f'{host}/{args.survey_id}/responses/bulk/?per_page=100'
+    url = f'{host}/{args.survey_id}/responses/bulk/?per_page=10'
     response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    response = response.json()
+    results.append(response)
+    r = 1
+    while 'next' in response['links'] and r < MAX_REQUESTS:
+        response = requests.get(response['links']['next'], headers=headers).json()
+        results.append(response)
+        r += 1
 else:
     raise ValueError(f'endpoint not supported: {args.endpoint}')
 
 if args.output:
-    pathlib.Path(args.output).parent.mkdir(parents=True, exist_ok=True)
-    with open(args.output, 'w') as f:
-        json.dump(response.json(), f)
+    out = Path(args.output)
+    if len(results) > 1:
+        out.mkdir(parents=True, exist_ok=True)
+        for i, response in enumerate(results):
+            fname = f'responses-{i:05}.json'
+            with open(Path(out, fname), 'w') as f:
+                json.dump(response, f)
+    else:
+        out.parent.mkdir(parents=True, exist_ok=True)
+        with open(out, 'w') as f:
+            json.dump(results[0], f)
 else:
-    print(response.json())
+    print(results[0])
